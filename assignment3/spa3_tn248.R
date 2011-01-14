11dc9554c44c0acfa83eaa27bbc7f0273f9de8ef
Thomson Nguyen <thomson@cantab.net>
2011-01-14 18:09:25 +0000

    added spa3

---
+++ assignment3/spa3_tn248.R
@@ -0,0 +1,10 @@
+dmin2d <- function(n, m, s, xlo, xhi, ylo, yhi){
+	## N: number of points to simulate
+	## m: mean of Normal distribution
+	## s: s.d. of Normal distribution
+	## xlo, xhi: possible range of X values.
+	## ylo, yhi: possible range of Y values.
+	
+	
+	
+	}
