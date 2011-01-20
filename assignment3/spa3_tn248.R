b2c849295ffc2a5166575df6a0fe0818897a2b8b
Thomson Nguyen <thomson@cantab.net>
2011-01-20 12:36:02 +0000

    submitted

--- assignment3/spa3_tn248.R
+++ assignment3/spa3_tn248.R
@@ -25,7 +25,7 @@ dmin2d <- function(n, m, s, xlo, xhi, ylo, yhi, plots=TRUE){
 	if(plots==TRUE){
 	plot(c(xlo-round(xlo*.05),xhi), c(ylo-round(xlo*.05), yhi), 
 		type = "n", xlab="", ylab="",main=paste("dmin(",
-		paste(n,m,s,xlo,ylo,xhi,yhi,sep=", "),")",sep=""))
+		paste(n,m,s,xlo,xhi,ylo,yhi,sep=", "),")",sep=""))
 	rect(xlo,ylo,xhi,yhi,lty=2)
 	points(thegoodones[,1],thegoodones[,2],pch=19)
 	  }
@@ -34,7 +34,7 @@ dmin2d <- function(n, m, s, xlo, xhi, ylo, yhi, plots=TRUE){
 	return(thegoodones)
 	}
 
-regind <- function(pointsdata){
+regind <- function(pointsdata,hist=TRUE){
 	## Takes a dataframe of points and calculates
 	## the regularity index.
 	##
@@ -47,11 +47,64 @@ regind <- function(pointsdata){
 	for(i in 1:dim(pointsdata)[1]){
 	mins <- c(mins,min(distmat[,i]))
 		}
-
+		
+	if(hist==TRUE){
+		hist(mins,xlab="Distance")
+		}
 	return(mean(mins)/sd(mins))
 }
 
-
+# PART I
 res <- dmin2d(200,30,5,200,1000,100,900)
 
+# PART II
+
+# square
 theoretical <- replicate(1000,regind(dmin2d(200,0,0,0,1000,0,1000,FALSE)))
+theoretical <- theoretical[rev(order(theoretical))]
+theoretical[50]
+
+# rectangle
+theoreticalrect <- replicate(1000,regind(dmin2d(200,0,0,0,500,0,2000,FALSE)))
+theoreticalrect <- theoreticalrect[rev(order(theoreticalrect))]
+theoreticalrect[50]
+
+# half points
+theoreticalhalf <- replicate(1000,regind(dmin2d(100,0,0,0,1000,0,1000,FALSE)))
+theoreticalhalf <- theoreticalhalf[rev(order(theoreticalhalf))]
+theoreticalhalf[50]
+
+theoreticalhalf2 <- replicate(1000,regind(dmin2d(50,0,0,0,1000,0,1000,FALSE)))
+theoreticalhalf2 <- theoreticalhalf2[rev(order(theoreticalhalf2))]
+theoreticalhalf2[50]
+
+# more points
+theoreticalmore <- replicate(1000,regind(dmin2d(3,0,0,0,1000,0,1000,FALSE)))
+theoreticalmore <- theoreticalmore[rev(order(theoreticalmore))]
+theoreticalmore[50]
+
+# PART III
+
+## load the real data
+real <- read.table("spa3_real.dat")
+plot(c(0,400), c(0,400), 
+		type = "n", xlab="", ylab="",main="Real Data")
+rect(0,0,400,400,lty=2)	
+points(real[,1],real[,2],pch=19)
+
+fitfinder <- function(m,s,real){
+	## calculates u-score for a given set of m,s parameters.
+	## n=238, xlo,ylo = 0, xhi,yhi = 400
+	## real= real dataset
+	uscore <- NULL
+	ffsimulated <- replicate(99,regind(dmin2d(150,m,s,0,400,0,400,FALSE)))
+	rri <- regind(real)
+	ris <- c(rri,ffsimulated)
+	
+	for(i in 1:100){
+		currentri <- ris[i]
+		ristemp <- ris[-i]
+		uscore[i] <- abs(currentri - sum(ristemp)/99)
+	}
+	return(uscore)
+}
